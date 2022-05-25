5e0793d52cdb16ce887ddd7a9f4245fdda906d38
khalil <khalillakhdharatc@gmail.com>
2022-05-25 10:42:45 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import  React  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props)
+    {
+        super(props);
+        this.state={
+             noms : ["Khalil "],
+             nm:'',
+             
+        }
+this.add=this.add.bind(this);
+this.onChange=this.onChange.bind(this);
+
+    }
+    onChange = e => { // arrow function
+        this.setState({ [e.target.name]: e.target.value });
+        };
+        add(event)
+        {
+            let ls=this.state.noms;
+            ls.push(this.state.nm);
+            console.log("liste",ls);
+            this.setState({noms:ls});
+            this.setState({nm:''});
+            //this.setState({noms:this.state.noms.push(this.state.nm)})
+            event.preventDefault();
+
+        }
+   
+    render()
+    {
+        return(
+            <><div>
+              <form onSubmit={this.add}>
+              <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Nom</label>
+  <input type="text" name="nm" value={this.state.nm} onChange={this.onChange} className="form-control"  placeholder="Votre nom ici" />
+  <input type="submit" className="btn btn-primary" value="Submit" />&nbsp;&nbsp;
+</div>
+
+              </form>
+                <ul>{this.state.noms.map((nom) => <li>{nom}</li>)}</ul>
+            </div>
+
+<div>
+                
+                
+                
+                </div></>
+        );
+    }
 }
-
 export default App;
